10f554cdb3ff67b93967f5ba0c1a037005dc7dac
akashc110 <akash.chitnis@gmail.com>
2016-11-14 23:33:42 -0500

    #Update

--- DM-1.R
+++ DM-1.R
@@ -4,12 +4,12 @@
 #INTRODUCTION
 #Benchmark is taken to be S&P500 index.
 #A five year period from Jan 2010 to Dec 2014 is considered to compute returns for the year 2015
-#Portfolio is rebalanced at the end of every month and transaction costs are taken into account
-#Four different portfolios are constructed based on market capitalization
-#Small, Medium, Big and a combination of these
+#Portfolio is rebalanced at the end of every month and transaction costs are taken into account (not yet included in the script)
+#Five different portfolios are constructed based on the size
+#12,24,36,48,60 are the respective number of assets in each portfolio
 #12 month period is considered to study the trade-off between tracking error and portfolio size
   #for these portfolios
-#Each portfolio is constructed using top 12 stocks by market capitalization in those segments
+#Each portfolio is constructed using top n stocks by market capitalization in small,mid & large segments
 
 #To see the download code script refer Symb_download.R file in directory
 
@@ -18,18 +18,24 @@ library(quadprog)
 smallcap <- read.csv("SmallCap.csv")[,-1]
 midcap <- read.csv("MidCap.csv")[,-1]
 largecap <- read.csv("LargeCap.csv")[,-1]
-mixcap <- cbind(smallcap[,1:5],midcap[,2:5],largecap[,2:5])
 
+Portfolio12 <- cbind(smallcap[,1:5],midcap[,2:5],largecap[,2:5])
+Portfolio24 <- cbind(smallcap[,1:9],midcap[,2:9],largecap[,2:9])
+Portfolio36 <- cbind(smallcap[,1:13],midcap[,2:13],largecap[,2:13])
+Portfolio48 <- cbind(smallcap[,1:17],midcap[,2:17],largecap[,2:17]) 
+Portfolio60 <- cbind(smallcap[,1:21],midcap[,2:21],largecap[,2:21])
+  
 Track = NULL
+Performance = NULL
+for (i in 1:5) {
 
-for (i in 1:4) {
-
-assets = 12
+assets = 12*i
 #cap <- smallcap
-if(i==1){cap <- smallcap}
-if(i==2){cap <- midcap}
-if(i==3){cap <- largecap}
-if(i==4){cap <- mixcap}
+if(i==1){cap <- Portfolio12}
+if(i==2){cap <- Portfolio24}
+if(i==3){cap <- Portfolio36}
+if(i==4){cap <- Portfolio48}
+if(i==5){cap <- Portfolio60}
 
 last = nrow(cap)
 
@@ -85,7 +91,9 @@ for (i in 1:12) {
 }
 d <- as.data.frame(d)     #Test year (2015) returns
 names(d) <- c("Benchmark_Returns", "Portfolio_Returns","Performance")
-table(d$Performance)
+
+z = table(d$Performance)
+Performance = rbind(Performance, z)
 
 Tracking.Error = mean((as.numeric(as.character(d$Portfolio_Returns) )  -  as.numeric(as.character(d$Benchmark_Returns)))^2 )*100
 Tracking.Error <- paste(as.character(round(Tracking.Error,3) ),"%")
@@ -94,7 +102,10 @@ Track = rbind(Track,Tracking.Error)
 
 table(beta1) 
  length(beta1[beta1!=0])
+ rm(returns.test, beta1 )
 }
 
-portfolio.name <- c("12 Small Cap", "12 Mid Cap", "12 Large Cap","12 Mix Cap")
-tab <- data.frame(Portfolio_Type = portfolio.name, Tracking_Error = Track)
+portfolio.name <- c("12 Assets", "24 Assets", "36 Assets","48 Assets","60 Assets")
+tab <- data.frame(Portfolio_Type = portfolio.name, Tracking_Error = Track,Performance)
+
+#Open tab from Environment to see the results
