e878817c7e7ec221432d92ada70753a521c38d84
akashc110 <akash.chitnis@gmail.com>
2016-12-17 11:37:00 -0500

    #CSV

--- ProfBellec.R
+++ ProfBellec.R
@@ -49,26 +49,79 @@ weights <- qp$solution
 range(qp$solution)
 
 ptf_weights <- function(bin, weights){
+  bin =30
   x = sample(1:150,size = bin, replace = T, prob =  weights)
-  print(x)
   wt = tabulate(x)/length(x)
+  table(wt)
+  
   return(data.frame(index = which(wt!=0), weights =wt[which(wt!=0)] )) 
 }
 
 #Code to calculate portfolio returns for 12 months of year 2015
-d = matrix(0,12,3)
-for (i in 1:12) {
-  portfolio = ptf_weights(30,weights)
-  returns.T <- returns.test[i,1]
-  returns.predict = returns.test[i,-1]
-  portfolio.returns <- t(as.matrix(portfolio$weights))%*%t(returns.predict[portfolio$index])
-  result <- ifelse(portfolio.returns>=returns.T,"Good","Poor")
-  d[i,] <- t(c(returns.T,portfolio.returns, result))
+size =500
+Parameters = matrix(ncol = 2, nrow = size)
+
+for (j in 1:size) {
   
+  d = matrix(0,12,4)  
+  
+  
+  for (i in 1:12) {
+      if(j<=100){portfolio = ptf_weights(30,weights)}
+      if(j>100){portfolio = ptf_weights(60,weights)}
+      if(j>200){portfolio = ptf_weights(90,weights)}
+      if(j>300){portfolio = ptf_weights(120,weights)}
+      if(j>400){portfolio = ptf_weights(135,weights)}
+      
+      companies <- nrow(portfolio)
+      returns.T <- returns.test[i,1]
+      returns.predict = returns.test[i,-1]
+      portfolio.returns <- t(as.matrix(portfolio$weights))%*%t(returns.predict[portfolio$index])
+      result <- ifelse(portfolio.returns>=returns.T,"Good","Poor")
+      d[i,] <- t(c(companies  ,returns.T,portfolio.returns, result))
+      
+  }
+  
+  df= as.data.frame(d)
+  names(df) <- c("Firms","Benchmark","Portfolio","Performance")
+  
+  Firms = as.numeric(as.character(df$Firms))
+  BM = as.numeric(as.character(df$Benchmark))
+  Ptf = as.numeric(as.character(df$Portfolio))
+  
+  Transaction.Cost = (mean(Firms))  
+  Tracking.Error =(mean(Ptf - BM)^2)  
+  
+  Parameters[j,] =  c(Transaction.Cost, Tracking.Error)
+
 }
 
-df= as.data.frame(d)
-names(df) <- c("Benchmark","Portfolio","Performance")
+Parameters = data.frame(Parameters)
+names(Parameters) = c("TransactionCost", "TrackingError")
+#plot(scale(Parameters$TransactionCost) ,scale(Parameters$TrackingError) )
+
+plot(Parameters$TrackingError, col = 'green', pch = 19,
+     main = "Transaction cost", 
+     xlab =  "Transaction Cost", ylab = "Tracking Error")
+lines(lowess(1:500,Parameters$TrackingError) , col='red')
+
+boxplot((Parameters$TransactionCost[1:100]),col = 'green',
+     main = 'Trade off between transaction cost and tracking error', 
+     xlab =  'Transaction Cost', ylab = 'Tracking Error')
+
+ggplot(Parameters, aes(factor(rbind(rep(1,100),rep(2,100),rep(3,100),rep(4,100),rep(5,100))),TrackingError)) + geom_boxplot() + xlab('portfolio sizes') +
+  ylab('Tracking Error') +  ggtitle("Variation of Tracking Error for 100 portfolio samples")
+
+ggplot(Parameters, aes(factor(rbind(rep(1,100),rep(2,100),rep(3,100),rep(4,100),rep(5,100))),Transaction.Cost)) + geom_boxplot() + xlab('portfolio sizes') +
+  ylab('Transaction Cost') +  ggtitle("Variation of Transaction Cost for 100 portfolio samples")
+
+
+
+
+
+
+
+
 
 
 
